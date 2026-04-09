26954e7ba2dcbfe8c0c5b10475c6d1937509e8e8
Junaid Rana <152472508+junaidrana12@users.noreply.github.com>
2026-04-09 22:11:56 +0530

    Add files via upload

---
+++ SmartAgricultureSystemApplication.java
@@ -0,0 +1,13 @@
+package com.smartagriculture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartAgricultureSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartAgricultureSystemApplication.class, args);
+	}
+
+}
