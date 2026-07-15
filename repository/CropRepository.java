38bad263c3d865c49acbb03f041d6b1e380e7933
Junaid Rana <152472508+junaidrana12@users.noreply.github.com>
2026-07-15 22:12:29 +0530

    Refactor CropRepository formatting and imports

--- repository/CropRepository.java
+++ repository/CropRepository.java
@@ -2,12 +2,9 @@ package com.smartagriculture.repository;
 
 import com.smartagriculture.entity.Crop;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 public interface CropRepository extends JpaRepository<Crop, Long> {
-
 //     List<Crop> findBySoilTypeAndSeason(String soil_type, String season);
-
      List<Crop> findBySoilTypeAndSeason(String soilType, String season);
 }
