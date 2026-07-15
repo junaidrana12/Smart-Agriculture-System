9469b22d0e6e3b11e5286ed309ecb37a4bc239ed
Junaid Rana <152472508+junaidrana12@users.noreply.github.com>
2026-07-15 22:11:12 +0530

    Refactor CropController formatting and imports

--- Controller/CropController.java
+++ Controller/CropController.java
@@ -1,12 +1,9 @@
 package com.smartagriculture.controller;
-
 import com.smartagriculture.entity.Crop;
 import com.smartagriculture.service.CropService;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @Controller
@@ -16,21 +13,20 @@ public class CropController {
     public CropController(CropService cropService) {
         this.cropService = cropService;
     }
-
+    
     @GetMapping("/")
-    public String home() {
+    public String home(){
         return "index";
     }
 
     @PostMapping("/recommend")
     public String recommend(@RequestParam("soil_type") String soilType,
                             @RequestParam String season,
-                            Model model) {
+                            Model model){
         System.out.println("SoilType = " + soilType);
-        System.out.println("Season = " + season);
-        List<Crop> crops = cropService.recommendCrops(soilType, season);
+         System.out.println("Season = " + season);
+          List<Crop> crops = cropService.recommendCrops(soilType, season);
         model.addAttribute("crops", crops);
-
         return "result";
     }
 }
